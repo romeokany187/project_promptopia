bd3099c0227026ebc71013fe810a188bca6083bf
romeokany187 <romeokany187@gmail.com>
2024-04-07 16:01:21 +0100

    modification

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,17 +1,17 @@
 "use client"
-import {useState} from 'react';
+import { useState } from 'react';
 import Image from 'next/image';
 import { useSession } from 'next-auth/react';
-import { usePathname , useRouter } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 
-const PromptCard = ({post , handleTagClick,handleEdit , handleDelete}) => {
-  const {data : session} = useSession();
+const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+  const { data: session } = useSession();
   const pathName = usePathname();
   const router = useRouter();
 
   const [copied, setCopied] = useState("");
 
-  const handleProfileClick = () =>{
+  const handleProfileClick = () => {
     console.log(post);
 
     if (post.creator._id === session?.user.id) return router.push("/profile");
@@ -28,11 +28,11 @@ const PromptCard = ({post , handleTagClick,handleEdit , handleDelete}) => {
       <div className="flex justify-between items-start gap-5">
         <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer" onClick={handleProfileClick}>
           <Image
-          src={post.creator.image}
-          alt='user_Image'
-          width={40}
-          height={40}
-          className='rounded-full object-contain' 
+            src={post.creator.image}
+            alt='user_Image'
+            width={40}
+            height={40}
+            className='rounded-full object-contain'
           />
           <div className="flex flex-col">
             <h3 className='font-satoshi font-semibold text-gray-900'>{post.creator.username} </h3>
@@ -52,8 +52,43 @@ const PromptCard = ({post , handleTagClick,handleEdit , handleDelete}) => {
           />
         </div>
       </div>
-      <p className='my-4 font-satoshi text-sm text-gray-700'>{post.prompt} </p>
-      <p className='font-inter text-sm blue_gradient cursor-pointer' onClick={()=> handleTagClick(post.tag)}> # {post.tag} </p>
+
+
+      <div className='cursor-pointer'>
+        <p className='my-4 font-satoshi text-sm text-gray-700 text-ellipsis text-nowrap overflow-hidden'
+          onClick={() => document.getElementById('my_modal_3').showModal()}>{post.prompt} </p>
+      </div>
+      {/* You can open the modal using document.getElementById('ID').showModal() method */}
+      <dialog id="my_modal_3" className="modal">
+        <div className="modal-box">
+          <form method="dialog">
+            {/* if there is a button in form, it will close the modal */}
+            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+          </form>
+          <div className="prompt_card border-none">
+            <div className="flex justify-between items-start gap-5">
+              <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer" onClick={handleProfileClick}>
+                <Image
+                  src={post.creator.image}
+                  alt='user_Image'
+                  width={40}
+                  height={40}
+                  className='rounded-full object-contain'
+                />
+                <div className="flex flex-col">
+                  <h3 className='font-satoshi font-semibold text-gray-900'>{post.creator.username} </h3>
+                  <p className='font-inter text-sm text-gray-500'>{post.creator.email} </p>
+                </div>
+              </div>
+              
+            </div>
+            <p className='my-4 font-satoshi text-sm text-gray-700'>{post.prompt} </p>
+            <p className='font-inter text-sm blue_gradient cursor-pointer' onClick={() => handleTagClick(post.tag)}> # {post.tag} </p>
+          </div>
+        </div>
+      </dialog>
+
+      <p className='font-inter text-sm blue_gradient cursor-pointer' onClick={() => handleTagClick(post.tag)}> # {post.tag} </p>
       {session?.user.id === post.creator._id && pathName === '/profile' && (
         <div className='mt-5 flex-center gap-4 border-t border-gray-100 pt-3'>
           <p className='font-inter text-sm green_gradient cursor-pointer' onClick={handleEdit}>
